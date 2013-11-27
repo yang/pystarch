97f2aaaa17a89c7b2088cac32aa025dba06d0b62
Chris Clark <clark800@gmail.com>
2013-11-27 05:18:22 -0500

    Moved Arguments class to expr module

--- expr.py
+++ expr.py
@@ -7,6 +7,81 @@ def get_token(node):
     return node.__class__.__name__
 
 
+def call_argtypes(call_node, context):
+    types = []
+    keyword_types = {}
+    for arg in call_node.args:
+        types.append(expression_type(arg, context))
+    for keyword in call_node.keywords:
+        keyword_types[keyword.arg] = expression_type(keyword.value, context)
+    if call_node.starargs is not None:
+        keyword_types['*args'] = expression_type(call_node.starargs, context)
+    if call_node.kwargs is not None:
+        keyword_types['**kwargs'] = expression_type(call_node.kwargs, context)
+    return types, keyword_types
+
+
+# "arguments" parameter is node.args for FunctionDef or Lambda
+class Arguments(object):
+    def __init__(self, arguments=None, context=None, decorator_list=[]):
+        if arguments is None:
+            return
+        if context is None:
+            context = Context()
+        self.names = [arg.id for arg in arguments.args]
+        self.min_count = len(arguments.args) - len(arguments.defaults)
+        default_types = [expression_type(d, context)
+                            for d in arguments.defaults]
+        self.default_types = ([Any()] * self.min_count) + default_types
+        self.explicit_types = self.get_explicit_argtypes(decorator_list)
+        self.types = [explicit if explicit != Any() else default
+            for explicit, default
+            in zip(self.explicit_types, self.default_types)]
+        self.vararg_name = arguments.vararg
+        self.kwarg_name = arguments.kwarg
+
+    @classmethod
+    def copy_without_first_argument(klass, other_arguments):
+        arguments = klass()
+        arguments.names = other_arguments.names[1:]
+        arguments.types = other_arguments.types[1:]
+        arguments.default_types = other_arguments.default_types[1:]
+        arguments.explicit_types = other_arguments.explicit_types[1:]
+        arguments.min_count = max(0, other_arguments.min_count - 1)
+        arguments.vararg_name = other_arguments.vararg
+        arguments.kwarg_name = other_arguments.kwarg
+        return arguments
+
+    def get_dict(self):
+        return dict(zip(self.names, self.types))
+
+    def get_explicit_argtypes(self, decorator_list):
+        types_decorator = [d for d in decorator_list
+            if get_token(d) == 'Call' and show_node(d.func) == 'types']
+        if len(types_decorator) == 1:
+            argtypes, kwargtypes = call_argtypes(decorator, context)
+        else:
+            argtypes, kwargtypes = [], {}
+        return argtypes + [kwargtypes.get(name, Any())
+            for name in self.names[len(argtypes):]]
+
+    def load_context(self, context):
+        for name, argtype in self.kwargtypes.items():
+            context.add_symbol(name, argtype)
+        if self.vararg_name:
+            context.add_symbol(self.vararg_name, Tuple())
+        if self.kwarg_name:
+            context.add_symbol(self.kwarg_name, Dict())
+
+    def __str__(self):
+        vararg = (', {0}: Tuple'.format(self.vararg_name)
+            if self.vararg_name else '')
+        kwarg = (', {0}: Dict'.format(self.kwarg_name)
+            if self.kwarg_name else '')
+        return (', '.join(name + ': ' + argtype for name, argtype
+            in zip(self.argnames, self.argtypes)) + vararg + kwarg)
+
+
 # adds assigned symbols to the current namespace, does not do validation
 def assign(target, value, context, generator=False):
     value_type = expression_type(value, context)
@@ -68,26 +143,19 @@ def expression_type(node, context):
     if token == 'BoolOp':
         return recur(node.values[0])
     if token == 'BinOp':
-        if get_token(node.op) in ['Add', 'Mult']:
-            if Str() in [recur(node.left), recur(node.right)]:
-                return Str()
-        return Num()
+        return Str() if (get_token(node.op) in ['Add', 'Mult']:
+            and Str() in [recur(node.left), recur(node.right)]) else Num()
     if token == 'UnaryOp':
-        if get_token(node.op) == 'Not':
-            return Bool()
-        else:
-            return Num()
+        return Bool() if get_token(node.op) == 'Not' else Num()
     if token == 'Lambda':
-        # TODO: enter arguments
-        arguments = None
-        return Function(arguments, recur(node.body))
+        return Function(Arguments(node.args, context), recur(node.body))
     if token == 'IfExp':
         return recur(node.body)
     if token == 'Dict':
-        if len(node.keys) > 0 and len(node.values) > 0:
-            return Dict(recur(node.keys[0]), recur(node.values[0]))
-        else:
-            return Dict(NoneType(), NoneType())
+        key_type = recur(node.keys[0]) if len(node.keys) > 0 else NoneType()
+        value_type = recur(node.values[0]) if len(node.values) > 0
+            else NoneType()
+        return Dict(key_type, value_type)
     if token == 'Set':
         return Set(recur(node.elts[0]))
     if token == 'ListComp':
@@ -122,10 +190,10 @@ def expression_type(node, context):
     if token == 'Name':
         return context.get_type(node.id, Undefined())
     if token == 'List':
-        return (List(recur(node.elts[0])) if len(node.elts) > 0
-            else List(NoneType()))
+        element_type = recur(node.elts[0]) if len(node.elts) > 0 else NoneType()
+        return List(element_type)
     if token == 'Tuple':
-        item_types = [recur(e) for e in node.elts]
+        item_types = [recur(element) for element in node.elts]
         return Tuple(item_types)
     raise Exception('evalute_type does not recognize ' + token)
 
